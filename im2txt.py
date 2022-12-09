19d8b196b8ab27c3bd65c4a0fe31a534494b61d4
Ivan Korec <korec@yandex.ru>
2022-12-09 15:27:45 +0300

    its works

--- im2txt.py
+++ im2txt.py
@@ -1,11 +1,30 @@
 from PIL import Image
 import pytesseract
+import os
+from dotenv import load_dotenv
+import telegram
+from telegram.ext import Updater, Filters, MessageHandler
+
+load_dotenv()
+TOKEN = os.getenv('BOT_TOKEN')
+updater = Updater(token=TOKEN)
 
 
 def img2txt(filename):
-    """ Возвращает текст с картинки """
+    '''Возвращает текст с картинки'''
     text = pytesseract.image_to_string(Image.open(filename), lang='rus+eng')
     return text
 
 
-print(img2txt('eng_pic.png'))
+def get_photo(update, context):
+    '''Скачивает фотографию от пользователя и отправлеят текст'''
+    file_id = update.message.photo[-1]
+    newFile = context.bot.getFile(file_id)
+    newFile.download('downloaded_img.jpg')
+    context.bot.sendMessage(chat_id=update.message.chat_id, text=img2txt('downloaded_img.jpg'))
+
+
+photo_handler = MessageHandler(Filters.photo, get_photo)
+updater.dispatcher.add_handler(photo_handler)
+updater.start_polling()
+updater.idle()
